d0adc321b1ad0ecb4c32a7ae2b6cbb232a6c3acb
Leo13 <leusbst@googlemail.com>
2022-01-11 12:16:37 +0100

    page-view-analyzer done

--- Python_Data_Analysis/page-view-time-series-visualizer/time_series_visualizer.py
+++ Python_Data_Analysis/page-view-time-series-visualizer/time_series_visualizer.py
@@ -29,9 +29,18 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = pass
+    df_bar = df.copy(deep=True)
 
     # Draw bar plot
+    df_bar = df.copy(deep=True)
+    df_bar['year'] = df_bar.index.year
+    df_bar['month'] = df_bar.index.month
+    df_bar = df_bar.groupby(["year","month"])['value'].mean()
+    #df_bar= df_bar.reset_index()#.sort_values(by=['year','month'])
+    df_bar= df_bar.unstack()
+
+    fig = df_bar.plot.bar(legend=True, figsize=(20,5),ylabel = "Average Page Views",xlabel= "Years").figure
+    plt.legend(['January','February','March','April','May','June','July','August','September','October','November','December'])
 
 
 
@@ -49,6 +58,22 @@ def draw_box_plot():
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
     # Draw box plots (using Seaborn)
+    fig, (ax1, ax2) = plt.subplots(1,2)
+    # fig.set_figheight(15)
+    # fig.set_figwidth(60)
+
+
+    sns.boxplot(ax=ax1, x="year", y= "value", data=df_box)
+    ax1.set_title("Year-wise Box Plot (Trend)")
+    ax1.set_xlabel("Year")
+    ax1.set_ylabel("Page Views")
+
+    month_order = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+
+    sns.boxplot(ax=ax2, x="month", y= "value", data=df_box, order = month_order)
+    ax2.set_title("Month-wise Box Plot (Seasonality)")
+    ax2.set_xlabel("Month")
+    ax2.set_ylabel("Page Views")
 
 
 
@@ -57,6 +82,3 @@ def draw_box_plot():
     # Save image and return fig (don't change this part)
     fig.savefig('box_plot.png')
     return fig
-
-    #https://replit.com/@GuidoPereyra/boilerplate-page-view-time-series-visualizer-1
-    #https://www.youtube.com/watch?v=889odedq2ww
