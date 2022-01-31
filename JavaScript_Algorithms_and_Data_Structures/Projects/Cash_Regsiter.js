1e87319335f46c2a7470ca4485e4af485c873bc9
Leo <leusbst@googlemail.com>
2022-01-31 17:11:36 +0000

    updated js project

--- JavaScript_Algorithms_and_Data_Structures/Projects/Cash_Regsiter.js
+++ JavaScript_Algorithms_and_Data_Structures/Projects/Cash_Regsiter.js
@@ -7,6 +7,71 @@ https://dev.to/frolovdev/how-to-operate-with-monetary-values-in-javascript-3fal
 
 
 
+const money ={
+  "PENNY":1,
+  "NICKEL":5,
+  "DIME":10,
+  "QUARTER":25,
+  "ONE":100,
+  "FIVE":500,
+  "TEN":1000,
+  "TWENTY":2000,
+  "ONE HUNDRED": 10000
+}
+
+function checkCashRegister(price, cash, cid){
+
+// define output variables
+  let status ="";
+  let change = [];
+
+  //define objects to substract from with "while>""
+  let claim= cash*100-price*100
+  let changeOut= claim
+
+  let cidTotal = 0;
+  //remove 0 values from cid
+  let filtered = cid.filter(unit=>unit[1]!==0).reverse();
+  // modify filtered and move values to substraction variables
+  filtered.forEach(unit =>{
+    let currencyUnit = unit[0];
+    let unitSum = unit [1]*100;
+    cidTotal+=unitSum;
+    let amount = 0;
+    //subtract values from variables and put them into change with amount
+    while(claim>=money[currencyUnit]&&unitSum>0){
+      amount += money[currencyUnit];
+      claim -= money[currencyUnit];
+      unitSum -= money[currencyUnit];
+    }
+    if (amount !== 0){
+      change.push([currencyUnit, amount / 100]);
+    }
+  });
+
+  if (claim>0){
+    status = "INSUFFICIENT_FUNDS";
+    change =[];
+  }
+  else if (claim ==0 && changeOut==cidTotal){
+    satus = "CLOSED";
+    change = cid;
+  }
+  else{
+    status = "OPEN";
+  }
+  return {'status':status, 'change':change};
+
+}
+
+
+console.log (checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90 ], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+console.log (checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+console.log (checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+console.log (checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+console.log (checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+
+
   const money ={
     "100.00": "ONE HUNDRED",
     "20.00": "TWENTY",
@@ -32,6 +97,7 @@ https://dev.to/frolovdev/how-to-operate-with-monetary-values-in-javascript-3fal
       change[key]= 0
         cidTotal+=n[1]
       })
+      console.log(cidTotal)
 
 
       let cidObject ={};
@@ -41,11 +107,11 @@ https://dev.to/frolovdev/how-to-operate-with-monetary-values-in-javascript-3fal
         cidObject[key] = value;
       })
 
+
       var cidOut = 0;
       for(var property in cidObject){
         cidOut+= cidObject[property];
       }
-
     // get amount of curreny units
     let numbers = Object.keys(money);
     // let unitList = []
@@ -84,71 +150,11 @@ https://dev.to/frolovdev/how-to-operate-with-monetary-values-in-javascript-3fal
         output.status ="OPEN",
         output.change = Object.entries(change).filter(n=>n[1]>0).reverse()
       }
-      return (output)
+      console.log(output)
   }
 
-// checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90 ], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90 ], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
 // checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
 // checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
 // checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
 // checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
-
-
-
-
-const currencyUnit ={
-  "PENNY":1,
-  "NICKEL":5,
-  "DIME":10,
-  "QUARTER":25,
-  "ONE":100,
-  "FIVE":500,
-  "TEN":1000,
-  "TWENTY":2000,
-  "ONE HUNDRED": 10000
-}
-
-function checkCashRegister(price, cash, cid){
-  let changeSum= cash*100-price*100
-  let changeSumCheck= changeSum
-  let change =[];
-  let status ='';
-
-  let cidSum = 0;
-  let filteredCid = cid.filter(elem=>elem[1]!==0).reverse();
-  filteredCid.forEach(elem =>{
-    let curr = elem[0];
-    let currSum = elem [1]*100;
-    cidSum+=currSum;
-    let amount = 0;
-    while(changeSum>=currencyUnit[curr]&&currSum>0){
-      amount += currencyUnit[curr];
-      changeSum -= currencyUnit[curr];
-      currSum -= currencyUnit[curr];
-    }
-    if (amount !== 0){
-      change.push([curr, amount / 100]);
-    }
-  });
-
-  if (changeSum>0){
-    status = "INSUFFICIENT_FUNDS";
-    change =[];
-  }
-  else if (changeSum ==0 && changeSumCheck==cidSum){
-    satus = "CLOSED";
-    change = cid;
-  }
-  else{
-    status = "OPEN";
-  }
-  return {'status':status, 'change':change};
-
-}
-
-
-console.log (checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90 ], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
-console.log (checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
-console.log (checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
-console.log (checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
-console.log (checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
