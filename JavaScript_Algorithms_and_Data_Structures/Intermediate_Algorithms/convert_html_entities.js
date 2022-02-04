69611d52532739cfc5bcb183332e2c3e065b07d7
Leo <leusbst@googlemail.com>
2022-02-04 11:47:09 +0000

    beu

--- JavaScript_Algorithms_and_Data_Structures/Intermediate_Algorithms/convert_html_entities.js
+++ JavaScript_Algorithms_and_Data_Structures/Intermediate_Algorithms/convert_html_entities.js
@@ -1,6 +1,30 @@
 // Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
 
 function convertHTML(str) {
+  var splitted = str.split("");
+
+  for (var i = 0; i < splitted.length; i++) {
+    switch (splitted[i]) {
+      case "<":
+        splitted[i] = "&lt;";
+        break;
+      case "&":
+        splitted[i] = "&amp;";
+        break;
+      case ">":
+        splitted[i] = "&gt;";
+        break;
+      case '"':
+        splitted[i] = "&quot;";
+        break;
+      case "'":
+        splitted[i] = "&apos;";
+        break;
+    }
+  }
+
+  splitted = splitted.join("");
+  return splitted;
 
 }
 
@@ -12,3 +36,5 @@ convertHTML('Stuff in "quotation marks"')
 convertHTML("Schindler's List")
 convertHTML("<>")
 convertHTML("abc")
+
+//using an object
